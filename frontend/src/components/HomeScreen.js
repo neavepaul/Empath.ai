56328c87b02b737b50e623429e040817c199b5ad
Neave Paul <me@neave.com>
2022-10-20 23:50:15 +0530

    grid layout modified

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -34,7 +34,6 @@ export default function HomeScreen() {
           <Halo/>
           
           <div className="sub-header">A virtual assistant that <br/> <span className="blue-text">cares</span></div>
-          {/* <div className="sub-headerbr"></div> */}
         </div>
       </section>
       <div className="about-us" id="about">
