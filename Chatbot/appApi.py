07d936d23e308f72497f6aadf73f4e23714f86b1
LAPTOP-0BE72HIK\afifk <afifkhanab@gmail.com>
2022-12-27 09:52:10 +0530

    UI done less fucking gooooo

--- Chatbot/appApi.py
+++ Chatbot/appApi.py
@@ -1,14 +1,18 @@
 from flask import Flask,request
 from collections import namedtuple
 from brain import chat
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
 
 @app.route('/')
 def home():
   return '<h1>Welcome to Empath.ai</h1>'
 
-@app.route('/api')
+@app.route('/api',methods = ['POST'])
 def api():
   user_input = request.get_json()
   response = chat(user_input)
--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios"
 
 export default function Chat() {
+  const [history,setHistory] = useState([{"type":"bot","content":"Hi , my name is Empath"}]);
+
+  function sendMessage(content){
+    fetchData(content);
+  }
+
+  async function fetchData(content) {
+    axios.post('http://127.0.0.1:5000/api', 
+    {'request': content })
+    .then(response => {
+      console.log(response.data);
+      setHistory(history.concat([{"type":"user","content":response.data.input},{"type":"bot","content":response.data.response}]));
+    });
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      sendMessage(event.target.value);
+      event.target.value = '';
+    }
+  }
+
   return (
     <div className='body'>
       <div className='chat-parent'>
-        <div className='bot-message message'>Hello lmao</div>    
-        <div className='user-message message'>I am sad as fuck</div>
+      {
+        history.map((msg)=>{
+          return (msg?.type === 'user')?
+          (<div className='user-message message'>{msg?.content}</div>):
+          (<div className='bot-message message'>{msg?.content}</div>);
+        })
+      }
       </div>
       <div className='center'>
-        <input className='chat-box' placeholder='Enter your message'></input>    
+        <input className='chat-box' placeholder='Enter your message' onKeyDown={(e)=>handleKeyDown(e)}></input>    
       </div>
     </div>
   )
