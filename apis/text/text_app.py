b254af18f61050f32186f771e33c0234d95c1abb
Neave Paul <37831381+neavepaul@users.noreply.github.com>
2023-05-05 22:10:32 +0530

    Add chatbot functionality

--- apis/text/text_app.py
+++ apis/text/text_app.py
@@ -1,15 +1,49 @@
 from flask import Flask, request, jsonify
-from transformers import pipeline
+from transformers import pipeline, Conversation
 
 app = Flask(__name__)
 
-classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
+# Load the emotion classifier model
+classifier = pipeline(
+    "text-classification",
+    model="j-hartmann/emotion-english-distilroberta-base",
+    return_all_scores=True
+)
 
-@app.route('/classify', methods=['POST'])
-def classify_text():
-    text = request.json['text']
-    result = classifier(text)
-    return jsonify(result)
+# Load the chatbot model
+chatbot = pipeline(
+    task="conversational",
+    model="facebook/blenderbot-400M-distill"
+)
+conversation = Conversation()
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    # Get input text from request
+    input_text = request.json['input_text']
+
+    # Predict the emotion of the input text
+    emotion_predictions = classifier(input_text)
+
+    # Add the input text to the conversation
+    conversation.add_user_input(input_text)
+
+    # Generate a response from the chatbot
+    chatbot_response = chatbot(conversation)
+
+    # Get the chatbot's response from the conversation
+    chatbot_output = chatbot_response.generated_responses[-1]
+    print(type(chatbot_output))
+
+    # Add the chatbot's response to the conversation
+    conversation.append_response(chatbot_output)
+
+    # Construct and return the JSON response
+    response = {
+        "emotion_predictions": emotion_predictions,
+        "response": chatbot_output,
+    }
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
