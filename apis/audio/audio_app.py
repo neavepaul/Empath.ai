f153bb6b15a78b9a971d4de24b2629931f32b552
Neave Paul <37831381+neavepaul@users.noreply.github.com>
2023-05-05 22:16:53 +0530

    Fixed port issues

--- apis/audio/audio_app.py
+++ apis/audio/audio_app.py
@@ -73,4 +73,4 @@ def classify():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
--- apis/text/text_app.py
+++ apis/text/text_app.py
@@ -46,4 +46,4 @@ def chat():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=3000, debug=True)
